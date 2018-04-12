9c658e9da5b3fd6fe7c4b0629575eda0652c5f0e
akshayp-webonise <akshay.pawar@weboniselab.com>
2018-04-12 13:19:40 +0530

    Basic To Do App created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import ListIterator from './ListIterator';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.userInput = React.createRef();
+  }
+
+  handleSubmit = event => {
+    let userInputElement = this.userInput.current;
+    let currentUserInput = userInputElement.value;
+    let items = [...this.state.items];
+    event.preventDefault();
+    if (currentUserInput !== '') {
+      if (items.indexOf(currentUserInput) > -1) {
+        alert('Duplicate Input');
+      } else {
+        items.push({
+          key: new Date().valueOf(),
+          text: currentUserInput
+        });
+        this.setState({
+          items
+        }, () => userInputElement.value = '');
+      }
+    } else {
+      alert('Enter something Jackass');
+    }
+  }
+
+  handleDelete = elementToBeDeleted => {
+    let items = this.state.items.filter(item => {
+      if (item.key !== elementToBeDeleted) {
+        return item;
+      }
+      return null;
+    });
+    this.setState({
+      items
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Basic App, Static Component</h1>
+      <div className='app'>
+        <form>
+          <input type='text' placeholder='Enter Items Here' ref={this.userInput} />
+          <button onClick={this.handleSubmit}>Add</button>
+        </form>
+        <ListIterator listItems={this.state.items} deleteItem={this.handleDelete} />
       </div>
     );
   }
